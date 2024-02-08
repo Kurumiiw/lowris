fa42e467fc672979c1f8bf871df7477165d39e4a
Ola Horg Jacobsen <olahja@stud.ntnu.no>
2024-02-08 22:41:59 +0100

    window

--- src/main.c
+++ src/main.c
@@ -1,10 +1,34 @@
-#include <stdio.h>
 #include <SDL.h>
+#include <stdbool.h>
 
 int main()
 {
+    bool running = true;
     SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow(
+            "lowris",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            1280,
+            720,
+            SDL_WINDOW_RESIZABLE
+            );
 
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    while(running)
+    {
+        SDL_Event e;
+        while(SDL_PollEvent(&e))
+        {
+            if(e.type == SDL_QUIT) running = false;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 95, 95, 95, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
 
     SDL_Quit();
     return 0;
