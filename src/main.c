03fcfd5789f40d3b03756717be882d3b42be30c3
Ola Horg Jacobsen <olahja@stud.ntnu.no>
2024-02-10 19:03:14 +0100

    proper behaviour

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "ns.h"
 
 typedef enum {
+    LOWRIS_NONE     = 0,
     LOWRIS_LEFT     = SDLK_LEFT,
     LOWRIS_RIGHT    = SDLK_RIGHT,
     LOWRIS_SLOW     = SDLK_DOWN,
@@ -22,12 +23,43 @@ typedef struct {
     bool running;
 
     lowris_current_tetromino *current_piece;
-    double move_timer;
-    double repeat_timer;
 
-    lowris_keymap held_key;
-    bool is_held;
+    double move_timer;
+
+    double repeat_timer;
+    double repeat_timer_threshold;
+
+    double repeat_wait_timer;
+    double repeat_wait_threshold;
+
+    lowris_keymap last_direction;
+
+    bool right_down;
+    bool left_down;
 } lowris_state;
+
+void LowrisMoveCurrentPieceLeft(lowris_state *state)
+{
+    if(state->current_piece->x > 0) state->current_piece->x--;
+}
+
+void LowrisMoveCurrentPieceRight(lowris_state *state)
+{
+    int32_t offset = state->current_piece->tetromino == I? 4 : 3;
+    if(state->current_piece->x < (BOARD_WIDTH - offset)) state->current_piece->x++;
+}
+
+void LowrisMoveCurrentPieceLeftmost(lowris_current_tetromino *current, lowris_board *board)
+{
+    current->x = 0;
+}
+
+void LowrisMoveCurrentPieceRightmost(lowris_current_tetromino *current, lowris_board *board)
+{
+    int32_t offset = current->tetromino == I? 4 : 3;
+
+    current->x = BOARD_WIDTH - offset;
+}
 
 int main()
 {
@@ -36,6 +68,8 @@ int main()
 
     state.running = true;
     uint64_t last_frame = ns();
+    bool right_down_last;
+    bool left_down_last;
 
     state.window = SDL_CreateWindow(
             "lowris",
@@ -46,15 +80,23 @@ int main()
             0
             );
 
-    state.renderer = SDL_CreateRenderer(state.window, -1, SDL_RENDERER_ACCELERATED);
+    state.renderer = SDL_CreateRenderer(state.window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     lowris_board *board = LowrisCreateBoard();
 
+    // Current piece setup
+
     state.current_piece = &(lowris_current_tetromino){
-        .tetromino = T,
+        .tetromino = I,
         .x = 3, .last_x = 3,
         .y = 0, .last_y = 0
     };
+
+    state.repeat_timer = 0;
+    state.repeat_timer_threshold = 0;
+
+    state.repeat_wait_timer = 0;
+    state.repeat_wait_threshold = 87;
 
     state.move_timer = 0;
 
@@ -73,59 +115,114 @@ int main()
 
             if(e.type == SDL_KEYDOWN)
             {
-                if (!state.is_held)
+                switch ((lowris_keymap)e.key.keysym.sym)
                 {
-                    state.is_held = true;
-
-                    //todo: repeat
-                    switch ((lowris_keymap)e.key.keysym.sym)
-                    {
-                        case LOWRIS_LEFT:
-                            state.current_piece->x -= 1;
-                            break;
-                        case LOWRIS_RIGHT:
-                            state.current_piece->x += 1;
-                            break;
-                        case LOWRIS_SLOW:
-                            break;
-                        case LOWRIS_ROT_CW:
-                            break;
-                        case LOWRIS_ROT_CCW:
-                            break;
-                        case LOWRIS_HARD:
-                            break;
-                        case LOWRIS_HOLD:
-                            break;
-                        case LOWRIS_ROT_180:
-                            break;
-                    }
-                    break;
+                    case LOWRIS_LEFT:
+                        state.left_down = true;
+                        break;
+                    case LOWRIS_RIGHT:
+                        state.right_down = true;
+                        break;
+                    case LOWRIS_SLOW:break;
+                    case LOWRIS_ROT_CW:break;
+                    case LOWRIS_ROT_CCW:break;
+                    case LOWRIS_HARD:break;
+                    case LOWRIS_HOLD:break;
+                    case LOWRIS_ROT_180:break;
+                    case LOWRIS_NONE:break;
                 }
             }
 
             if(e.type == SDL_KEYUP)
             {
-                state.is_held = false;
+                switch ((lowris_keymap)e.key.keysym.sym)
+                {
+                    case LOWRIS_LEFT:
+                        state.left_down = false;
+                        break;
+                    case LOWRIS_RIGHT:
+                        state.right_down = false;
+                        break;
+                    case LOWRIS_SLOW:break;
+                    case LOWRIS_HARD:break;
+                    case LOWRIS_HOLD:break;
+                    case LOWRIS_ROT_CW:break;
+                    case LOWRIS_ROT_CCW:break;
+                    case LOWRIS_ROT_180:break;
+                    case LOWRIS_NONE:break;
+                }
             }
         }
 
         // --- LOGIC
 
-        state.move_timer += delta_time;
-
-        if(state.is_held)
+        // RIGHT was just pressed
+        if(state.right_down && !right_down_last)
         {
+            state.repeat_wait_timer = 0;
+            state.repeat_timer = 0;
 
+            LowrisMoveCurrentPieceRight(&state);
+            state.last_direction = LOWRIS_RIGHT;
         }
+
+        // LEFT was just pressed
+        if(state.left_down && !left_down_last)
+        {
+            state.repeat_wait_timer = 0;
+            state.repeat_timer = 0;
+
+            LowrisMoveCurrentPieceLeft(&state);
+            state.last_direction = LOWRIS_LEFT;
+        }
+
+        // both LEFT and RIGHT are being pressed
+        if(state.left_down && state.right_down)
+        {
+            switch(state.last_direction)
+            {
+                case LOWRIS_LEFT:
+                    goto LEFT_MOVEMENT_LOGIC;
+                case LOWRIS_RIGHT:
+                    goto RIGHT_MOVEMENT_LOGIC;
+            }
+        }
+        //both are not being pressed, left is held down
+        else if(state.left_down)
+        {
+            LEFT_MOVEMENT_LOGIC:
+            state.repeat_wait_timer += delta_time;
+            if(state.repeat_wait_timer > state.repeat_wait_threshold)
+            {
+                //CHECK speed setting
+                LowrisMoveCurrentPieceLeftmost(state.current_piece, board);
+            }
+        }
+        // oth are not being pressed, right is held down
+        else if(state.right_down)
+        {
+            RIGHT_MOVEMENT_LOGIC:
+            state.repeat_wait_timer += delta_time;
+            if(state.repeat_wait_timer > state.repeat_wait_threshold)
+            {
+                //CHECK speed setting
+                LowrisMoveCurrentPieceRightmost(state.current_piece, board);
+            }
+        }
+
+        state.move_timer += delta_time;
 
         if(state.move_timer >= 500) //todo: update based on level
         {
-            state.current_piece->y += 1;
+            //state.current_piece->y += 1;
 
             state.move_timer = 0;
         }
 
         LowrisUpdateCurrentTetromino(board, state.current_piece, state.renderer);
+
+        left_down_last = state.left_down;
+        right_down_last = state.right_down;
 
         // --- RENDER
 
